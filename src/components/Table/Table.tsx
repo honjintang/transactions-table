2c16e1e39cd95f4bbde2f3605943d5f23dfed9ad
Vivien Tang <vivien.tang@itv.com>
2024-10-21 09:50:49 +0100

    fix: correct table ids

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -17,18 +17,18 @@ const Table = ({ rows }: TableProps) => {
           <table>
           <thead>
             <tr>
-              <th id="column-id" key="id">ID</th>
-              <th id="column-date" key="id">Date</th>
-              <th id="column-amount" key="id">Amount</th>
-              <th id="column-merchant" key="id">Merchant</th>
-              <th id="column-category" key="id">Category</th>
+              <th id="column-id" key="column-id">ID</th>
+              <th id="column-date" key="column-date">Date</th>
+              <th id="column-amount" key="column-amount">Amount</th>
+              <th id="column-merchant" key="column-merchant">Merchant</th>
+              <th id="column-category" key="column-category">Category</th>
             </tr>
           </thead>
           <tbody>
             {rows.map((row) => (
               <tr key={row.id}>
                 <td headers="column-id" key={`${row.id}-id`}>{row.id}</td>
-                <td headers="column-date" key={`${row.date}-date`}>{formatDate(row.date)}</td>
+                <td headers="column-date" key={`${row.id}-date`}>{formatDate(row.date)}</td>
                 <td headers="column-amount" key={`${row.id}-amount`}>{`£${row.amount}`}</td>
                 <td headers="column-merchant" key={`${row.id}-merchant`}>{row.merchant}</td>
                 <td headers="column-category" key={`${row.id}-category`}>{`${row.category.charAt(0).toUpperCase() + row.category.slice(1)}`}</td>
